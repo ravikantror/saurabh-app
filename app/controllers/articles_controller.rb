771709b2a2c9038695590955b39d3228a66c83a7
CCS-06 <ccs-06@CCS-06.(none)>
2016-07-02 12:46:39 +0530

    Task with AJAX

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,13 +1,11 @@
 class ArticlesController < ApplicationController
-  def index
+    def index
     if params[:_method].present?
-      #@articles = Article.all
       @articles = current_user.articles
     else
       @articles = Article.all
-      #@articles = current_user.articles
     end
-  	
+    
   end
 
   def show
@@ -15,40 +13,43 @@ class ArticlesController < ApplicationController
   end
 
   def new
-  	@article = Article.new
+    @article = Article.new
   end
 
   def edit
-  	@article = Article.find(params[:id])
+    @article = Article.find(params[:id])
   end
 
   def create
     @article = current_user.articles.new(article_params)
     #@article = Article.new(article_params)
     #@article.user_id = current_user.id
-  	@article.save
-  	redirect_to @article
+    @article.save
+    # redirect_to articles_path
+     @articles = Article.all
   end
 
   def update
-  		@article = Article.find(params[:id])
+      @article = Article.find(params[:id])
  
-  	if @article.update(article_params)
-    	redirect_to @article
-  	else
-    	render 'edit'
-  	end
+    if @article.update(article_params)
+      #redirect_to articles_path
+      @articles = Article.all
+    else
+      render 'edit'
+    end
   end
  
   def destroy
-  	@article = Article.find(params[:id])
-  	@article.destroy
+    @article = Article.find(params[:id])
+    @article.destroy
  
-  	redirect_to articles_path
+    redirect_to articles_path
   end
- 
+    
 private
   def article_params
     params.require(:article).permit(:user_id, :title, :text, :post_type)
   end
+
 end
